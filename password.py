a4ffa6823092e1fef22ef14d3589576147997ba4
Ange Uwimana <uuwange@gmail.com>
2020-11-15 04:47:20 +0200

    Introduced an empty password locker

--- password.py
+++ password.py
@@ -2,16 +2,17 @@ class User:
     """
     Class that generates new instances of user
     """
+    password_locker = []
  def __init__(self,login_username,password):
     
     self.login_username = login_username
     self.password = password
     
- __init__ method that helps us define properties for our objects.
+#  __init__ method that helps us define properties for our objects.
 
-        Args:
-        login_username: New login_username.
-        password : New password.
+#         Args:
+#         login_username: New login_username.
+#         password : New password.
 
     pass
 
