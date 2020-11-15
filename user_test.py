2b7200a3db9b3b6b30a1e64adc03d28ca94a6fd4
Ange Uwimana <uuwange@gmail.com>
2020-11-15 20:40:05 +0200

    created the class of user and credentials

---
+++ user_test.py
@@ -0,0 +1,46 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.login_username = login_username("Ange")
+        self.password = password("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
