ad63302b7296e82a123fd367476b44a8f4400f33
Ange Uwimana <uuwange@gmail.com>
2020-11-15 04:23:11 +0200

    Created the Classes for the User and Credentials

---
+++ password.py
@@ -0,0 +1,47 @@
+class User:
+    """
+    Class that generates new instances of user
+    """
+
+    pass
+
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
