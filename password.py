cb633c95490fd0860f75de995b2c94c04b66b289
Ange Uwimana <uuwange@gmail.com>
2020-11-15 04:36:11 +0200

    Created _init_method for the new instances of a class

--- password.py
+++ password.py
@@ -2,7 +2,11 @@ class User:
     """
     Class that generates new instances of user
     """
-
+ def __init__(self,login_username,password):
+    
+    self.login_username = login_username
+    self.password = password
+    
     pass
 
 class Credentials:
