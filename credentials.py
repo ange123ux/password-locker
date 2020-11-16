b3b4a545e8cf50f9e190b5050969f0c305f24e66
Ange Uwimana <uuwange@gmail.com>
2020-11-16 12:10:00 +0200

    fixed errors

---
+++ credentials.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3.6
+from user import User
+from user import Account
+def create_user(first_name, last_name, login_username, password):
+    new_user = User(first_name, last_name, login_username, password)
+    return new_user
+def save_user(user):
+    user.save_user()
+def delete_user(user):
+    user.delete_user()
+# def find_user(username):
+#     return User.find_by_login_username(username)
+def find_password(userpassword):
+    return User.find_by_userpassword(userpassword)
+def display_user():
+    return User.display_userInfo()
+def create_account(account_name,account_username,account_password):
+    new_account = Account(account_name,account_username,account_password)
+    return new_account
+def save_account(user):
+    user.save_account()
+def delete_account(user):
+    user.delete_account()
+def display_account_credentials():
+    return Account.display_accounts()
+def find_account_credentials(account_name):
+    return Account.find_by_accountName(account_name)
+def main():
+    print("Enter your name")
+    name = input()
+    print(f"Hey {name} choose one of the options below:")
+    print('\n')
+    while True:
+        print("Use the following code to signup: su - SignUp, nw - Add New Account, da - Display Accounts, vw -View Account Details, dl - Delete Account Detaiils")
+        user_choice = input().lower()
+        if user_choice == "su":
+            print("Create an account")
+            print("-"*8)
+            print("Enter Username:")
+            username = input()
+            print("Enter password")
+            password = input()
+            save_account(create_account(username, password))
+            print('\n')
+            print(f"New user {login_userName} has been created.You can now proceed to login with your credentials.")
+            print('\n')
+        # elif user_choice == "lg":
+        #     print("Enter your Username:")
+        #     login_userName = input()
+        #     print("Enter your password:")
+        #     login_password = input()
+        #     if find_user(login_userName) and find_password(login_password):
+        #         print('\n')
+        #         print("Welcome!To continue please choose any of the options below:")
+        #         print("-"*8)
+        #         print("nw - Add New Account, da - Display Accounts, vw -View Account Details, dl - Delete Account Detaiils")
+        #         account_choice = input().lower()
+        #         if account_choice == "nw":
+        #             print("Enter Account Name:")
+        #             accountname = input()
+        #             print("Enter Account Username:")
+        #             accountusername = input()
+        #             print("Enter Account Password:")
+        #             accountpassword = input()
+        #             save_account(create_account(accountname,accountusername,accountpassword))
+        #             print(f"Account Name:{accountname}, Account Username:{accountusername}, Account Password:{accountpassword}")
+        #         elif account_choice == "da":
+        #             if display_account_credentials():
+        #                 print("List of all your accounts: ")
+        #                 print('\n')
+        #                 for account in display_account_credentials():
+        #                     print(f"Account Name:{account.account_name}")
+        #             else:
+        #                 print("invalid choice")
+        #         elif account_choice == "vw":
+        #             print("Enter an Account Name:")
+        #             account_choiceName = input()
+        #             if display_account_credentials():
+        #                 account_choiceName = find_account_credentials(account_choiceName)
+        #                 print(f"Account Username:{account_choiceName.account_userName}   Password:{account_choiceName.account_password}")
+        #             else:
+        #                 print(f"{account_choiceName} does not exist")
+        #         elif account_choice == "dl":
+        #             print("Enter Account Name:")
+        #             delete_acc = input()
+        #             if delete_account(delete_acc):
+        #                 return delete_account(delete_acc)
+        #             else:
+        #                 print(f"{delete_acc} does not exist")
+        #     else:
+        #         print("Wrong username or password. Please try again.")
+        #         print('\n')
+        # else:
+        #     print("Incorrect Option. Please choose from the ones listed")
+        #     print('\n')
+if __name__ == '__main__':
+    main()
--- user_test.py
+++ user_test.py
@@ -1,22 +1,112 @@
 import unittest
 from user import User
-
+from user import Account
 class TestUser(unittest.TestCase):
-
     '''
     Test class that defines test cases for the user class behaviours.
-
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
-def setUp(self):
+    def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.login_username = login_username("Ange")
-        self.password = password("uwimana")
+        self.new_user = User("Angel", "Uwimana", "Angel", "angel2020")
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name, "Angel")
+        self.assertEqual(self.new_user.last_name, "Uwimana")
+        self.assertEqual(self.new_user.login_username, "Angel")
+        self.assertEqual(self.new_user.password, "angel2020")
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the user listed
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+    def test_save_multiple_users(self):
+        '''
+        test_save_multiple_users to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Doria", "Igiraneze","Doria", "Doria2020")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = User("Angelique", "Mizero", "Angelique","Angelique2020")
+        test_user.save_user()
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)
+
+    # def test_find_user_by_username(self):
+    #     '''
+    #     test to check if we can find a user by their username and display information
+    #     '''
+    #     self.new_user.save_user()
+    #     test_user = User("Angelique", "Mizero", "Angelique", "Angelique2020")
+    #     test_user.save_user()
+    #     found_user = User.find_by_login_username("Angelique")
+        
+    #     self.assertEqual(find_user.login_username,test_user.login_username)
+    def test_find_user_by_password(self):
+        '''
+        test to check if we can find a user by their password
+        '''
+        self.new_user.save_user()
+        test_user = User("Ange", "Uwimana", "Ange", "ange2020")
+        test_user.save_user()
+        found_password = User.find_by_userpassword("ange2020")
+        self.assertEqual(found_password.password,"ange2020")
+
+    def test_display_user_information(self):
+        '''
+        test to check if we can be able to display users saved in user_list
+        '''
+        self.assertEqual(User.display_userInfo(),User.user_list)
+
+class TestAccount(unittest.TestCase):
+    def setUp(self):
+        self.new_account = Account("Instagram", "Uwimana", "ange2020")
+    def test_init(self):
+        self.assertEqual(self.new_account.account_name,"Instagram")
+        self.assertEqual(self.new_account.account_username,"Uwimana")
+        self.assertEqual(self.new_account.account_password,"ange2020")
+    def test_save_account(self):
+        self.new_account.save_account()
+        self.assertEqual(len(Account.account_list),1)
+
+    def tearDown(self):
+        Account.account_list = []
+    def test_display_account_credentials(self):
+        self.assertEqual(Account.display_accounts(),Account.account_list)
+
+    def test_delete_account(self):
+        self.new_account.save_account()
+        test_account = Account("Twitter", "angeange", "angeange2020")
+        test_account.save_account()
+        test_account.delete_account()
+        self.assertEqual(len(Account.account_list),1)
+
+    def test_find_account_by_name(self):
+        self.new_account.save_account()
+        test_account = Account("Facebook", "uwimana", "1234567")
+        test_account.save_account()
+        found_account = Account.find_by_accountName("Facebook")
+        self.assertEqual(found_account.account_name,"Facebook")
+if __name__ == '__main__':
+    unittest.main()
 
 
 
